a78768fac550d347eb7637faec0dfda486fbe115
Vinicius Zuppa <vcordeiro12@gmail.com>
2024-04-16 19:44:15 -0300

    feat: Adicionar estrutura inicial do projeto

---
+++ src/main/java/br/com/alura/AluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApplication.class, args);
+	}
+
+}
