1cc257530f0fa75fe2c684a92f173e8a0da332d5
Vinicius Zuppa <vcordeiro12@gmail.com>
2024-04-18 09:32:50 -0300

    feat(course_evaluation): Adiciona novo campo na tabela course_evaluation

--- src/main/resources/db/migration/V004__create_table_course_evaluation.sql
+++ src/main/resources/db/migration/V004__create_table_course_evaluation.sql
@@ -4,6 +4,7 @@ CREATE TABLE course_evaluation (
     course_id INTEGER NOT NULL,
     score INTEGER NOT NULL,
     reason VARCHAR(255) NOT NULL,
+    evaluation_date DATE NOT NULL,
     CONSTRAINT fk_course_evaluation_user FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_course_evaluation_course FOREIGN KEY (course_id) REFERENCES courses (id),
     PRIMARY KEY (id)
